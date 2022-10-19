761d70870c4a3fca6bbe40bcf70e3b7c91df5328
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-10-19 18:33:32 -0400

    Get shells base address and shells pointer address

--- EtGEssentials.cpp
+++ EtGEssentials.cpp
@@ -7,4 +7,8 @@ int main()
 
     Address health_base_addr = memory.getModule("UnityPlayer.dll");
     Address health_addr = memory.getAddress(health_base_addr + 0x0146FCC8, { 0x120, 0x10, 0x30, 0x30, 0x38, 0x28, 0x118 });
+
+    Address shells_base_addr = memory.getModule("mono.dll");
+    Address shells_addr = memory.getAddress(shells_base_addr + 0x00264A68, { 0xC0, 0xE90, 0xA8, 0x198, 0x1C });
+    // shells are the currency used in EtG runs if you didn't know
 }
