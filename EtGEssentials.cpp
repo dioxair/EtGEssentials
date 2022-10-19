902f22089f221e9af233b82184644674fb3d494a
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-10-19 18:30:31 -0400

    Renamed "base_addr" to "health_base_addr"

--- EtGEssentials.cpp
+++ EtGEssentials.cpp
@@ -5,6 +5,6 @@ int main()
 {
     Memory::External memory = Memory::External("EtG.exe", true);
 
-    Address base_addr = memory.getModule("UnityPlayer.dll");
-    Address health_addr = memory.getAddress(base_addr + 0x0146FCC8, { 0x120, 0x10, 0x30, 0x30, 0x38, 0x28, 0x118 });
+    Address health_base_addr = memory.getModule("UnityPlayer.dll");
+    Address health_addr = memory.getAddress(health_base_addr + 0x0146FCC8, { 0x120, 0x10, 0x30, 0x30, 0x38, 0x28, 0x118 });
 }
