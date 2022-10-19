d0c42c4cb34c9560c8f9558f4c5076c1fde18725
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-10-19 18:27:00 -0400

    Get module base address and health pointer address

--- EtGEssentials.cpp
+++ EtGEssentials.cpp
@@ -3,5 +3,8 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Memory::External memory = Memory::External("EtG.exe", true);
+
+    Address base_addr = memory.getModule("UnityPlayer.dll");
+    Address health_addr = memory.getAddress(base_addr + 0x0146FCC8, { 0x120, 0x10, 0x30, 0x30, 0x38, 0x28, 0x118 });
 }
