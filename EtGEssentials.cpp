725c85803f640469024f7f453fb391905f36765a
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-10-20 06:33:33 -0400

    Added console menu with no functionality

--- EtGEssentials.cpp
+++ EtGEssentials.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 int main()
 {
+    int choice;
+    bool loop_running = true;
+
     Memory::External memory = Memory::External("EtG.exe", true);
 
     Address health_base_addr = memory.getModule("UnityPlayer.dll");
@@ -16,4 +19,36 @@ int main()
 
     float current_health = memory.read<float>(health_addr);
     float current_shells = memory.read<int>(shells_addr);
+
+    while (loop_running != false) {
+        cout << "EtG Essentials\n";
+        cout << " 1 - Infinite Health\n";
+        cout << " 2 - Infinite Shells\n";
+        cout << " 3 - Exit.\n";
+        cout << " Enter your choice and press return: ";
+
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            cout << "\nGave Player infinite health!\n\n";
+            // write to health pointer in a loop
+            break;
+        case 2:
+            cout << "\nGave Player 100 million shells!\n\n";
+            // write to shells pointer once
+            break;
+        case 3:
+            cout << "\nExiting..";
+            Sleep(2000);
+            loop_running = false;
+            return 0;
+            break;
+        default:
+            cout << "Not a valid choice, choose again.";
+            break;
+        }
+
+    }
+    return 0;
 }
