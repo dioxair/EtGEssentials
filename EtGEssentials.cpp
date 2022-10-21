f5c56ac9907f536e444bb09f2ab7c9cf621c864d
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-10-21 17:45:40 -0400

    Added infinite blanks cheat

--- EtGEssentials.cpp
+++ EtGEssentials.cpp
@@ -41,6 +41,23 @@ void infinite_health(Memory::External memory, Address health_addr)
     }
 }
 
+void infinite_blanks(Memory::External memory, Address blanks_addr)
+{
+	while (true)
+	{
+        Sleep(100);
+        memory.write<int>(blanks_addr, 10, true);
+        // if EtGEssentials is focused
+        if (GetConsoleWindow() == GetForegroundWindow())
+        {
+            if (GetAsyncKeyState(VK_ESCAPE))
+            {
+                exit(0);
+            }
+        }
+	}
+}
+
 int WINAPI _tWinMain(HINSTANCE hinstance, HINSTANCE hPrevinstance, LPTSTR lpszCmdLine, int nCmdShow);
 
 int main()
@@ -65,17 +82,23 @@ int main()
     Address shells_addr = memory.getAddress(shells_base_addr + 0x00264A68, { 0xC0, 0xE90, 0xA8, 0x198, 0x1C });
     // shells are the currency used in EtG runs if you didn't know
 
+    Address blanks_base_addr = memory.getModule("UnityPlayer.dll");
+    Address blanks_addr = memory.getAddress(blanks_base_addr + 0x0144EBB8, { 0x8, 0x98, 0x28, 0x30, 0x568 });
+
     float current_health = memory.read<float>(health_addr);
     int current_shells = memory.read<int>(shells_addr);
+    int current_blanks = memory.read<int>(blanks_addr);
 
     while (true) {
         cout << "Current health: " << current_health << endl;
         cout << "Current shells: " << current_shells << endl;
+        cout << "Current blanks: " << current_blanks << endl;
 
         cout << "\nEtG Essentials\n";
         cout << " 1 - Infinite Health\n";
         cout << " 2 - Infinite Shells\n";
-        cout << " 3 - Exit.\n";
+        cout << " 3 - Infinite Blanks\n";
+        cout << " 4 - Exit.\n";
         cout << " Enter your choice and press return: ";
 
         cin >> choice;
@@ -90,6 +113,9 @@ int main()
             cout << "\nGave Player 100 million shells!\n\n";
             break;
         case 3:
+            cout << "\nGiving Player infinite blanks! Press the ESCAPE key to exit EtGEssentials\n\n";
+            infinite_blanks(memory, blanks_addr);
+        case 4:
             cout << "\nExiting..";
             Sleep(2000);
             return 0;
